737d00c265e1d2d0dc36d5a6d34feb69f251c184
unknown <A00193644@V103-A18.studentnet.ait.ie>
2015-10-22 12:44:21 +0100

    Created Main.cpp and display a small window

---
+++ SolarSystem/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <Windows.h>
+
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello, world!");
+
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+		std::cerr<<"Error: "<<glewGetErrorString(res)<<"\n";
+		return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
+    glutMainLoop();
+    
+    return 0;
+}
